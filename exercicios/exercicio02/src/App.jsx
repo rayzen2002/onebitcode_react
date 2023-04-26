e53c0494d07944d607e28913d047aa0f9f45897e
Emanuel <emanunelzsmj@gmail.com>
2023-04-26 10:56:45 -0300

    Props

--- exercicios/exercicio02/src/App.jsx
+++ exercicios/exercicio02/src/App.jsx
@@ -1,9 +1,14 @@
-import Card from "./components/Card";
+import Card from "./components/Card/Card";
+import posterImgSw from "./assets/sw-poster.jpg"
+import posterImgEsb from "./assets/esb-poster.jpg"
+import posterImgRotj from "./assets/rotj-poster.jpg"
 
 export default function App(){
   return (
     <>
-      <Card />
+      <Card title = "Pôster: Star Wars (1977)" posterImg = {posterImgSw}/>
+      <Card title = "Pôster: Empire strikes back (1980)" posterImg = {posterImgEsb}/>
+      <Card title = "Pôster: Return of the Jedi (1983)" posterImg = {posterImgRotj}/>
     </>
   )
 }
