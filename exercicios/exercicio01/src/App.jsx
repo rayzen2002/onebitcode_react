5c39c99a4ef4db1a76bfad4a0f6457eef0be9e6b
Emanuel <emanunelzsmj@gmail.com>
2023-04-25 23:14:15 -0300

    Exercicio criando pagina com React/Vite

---
+++ exercicios/exercicio01/src/App.jsx
@@ -0,0 +1,30 @@
+export default function App(){
+  return (
+    <div>
+      <img src="./react.png" alt="react" width={`150vh`} />
+    <h1>React</h1>
+    <p>A biblioteca para interfaces de usuarios web e nativas</p>
+    <button>Aprenda React</button>
+    <button>Referencia da API</button>
+    <hr />
+    <h2>Crie interfaces de usuario de componentes</h2>
+    <p>React permite que voce construa interfaces de usuario a partir de pedacos individuais chamados componentes</p>
+    <hr />
+    <h2>Escreva componentes com codigo e marcacao</h2>
+    <p>Componentes React sao funcoes Javascript.A sintaxe de marcacao e chamada de JSX.E uma extensao da sintaxe
+      de Javascript popularizada pelo React.
+    </p>
+    <hr />
+    <h2>Proximos passos</h2>
+    <ul>
+      <li>Uso de dados dinamicos no JSX</li>
+      <li>Criacao de novos componentes</li>
+      <li>Estilizacao de componentes</li>
+      <li>Reutilizacao de componentes</li>
+      <li>Uso de props e Children</li>
+      <li>Uso de eventos Javascript</li>
+    </ul>
+    </div>
+
+  )
+}
