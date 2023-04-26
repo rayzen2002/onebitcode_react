9f0d251f94f7f196e956ec4e0b6c6ae0def802ca
Emanuel <emanunelzsmj@gmail.com>
2023-04-25 23:00:49 -0300

    Criando projeto React com o Vite

---
+++ vite/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  
+  return ( <div>
+    <h1>Hello, world</h1>
+    <p>React + Vite</p>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+    </ul>
+  </div>)
+}
+
+export default App
